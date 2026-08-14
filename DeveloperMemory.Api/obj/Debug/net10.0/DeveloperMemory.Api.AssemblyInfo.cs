b590938e15111cb2e226cc8b051d3545f258895b
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2026-08-14 09:26:19 +0330

    fix

--- DeveloperMemory.Api/obj/Debug/net10.0/DeveloperMemory.Api.AssemblyInfo.cs
+++ DeveloperMemory.Api/obj/Debug/net10.0/DeveloperMemory.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeveloperMemory.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efa5876dd1e20984669f9a77181e7f0bb3f7acb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6270489d16b54bcf1c2ad5b3cd628a5cbb72862")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeveloperMemory.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeveloperMemory.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
